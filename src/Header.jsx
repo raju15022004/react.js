6ab6b3868571ccf3f338f1392a9d9cb8c091ae72
raju15022004 <raju032004@gmail.com>
2025-08-05 22:10:18 +0530

    App.js

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,10 @@
-function Header(){
+function Header(headerInfo,cname){
+  // console.log(props)
   return(
     <div>
-    <h1>Welcome to Header section </h1>
-    <div>hello wscubetech</div>
-    <div>hello Wscubetech</div>
+      <h3>header part</h3>
+     <h1>{headerInfo.email} {cname} </h1>
+
 </div>
   )
 }
