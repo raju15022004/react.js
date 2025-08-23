7d1e2e3ccac3b7e7bb8a386650fe746b2579dbac
raju15022004 <raju032004@gmail.com>
2025-08-24 01:10:48 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
-import Footer from './Footer';
-import {Container} from 'react-bootstrap';
-import { Row, Col } from 'react-bootstrap';
+import { useState } from 'react';
+// import Footer from './Footer';
+// import {Container} from 'react-bootstrap';
+// import { Row, Col } from 'react-bootstrap';
 // import { Row, Col } from 'antd';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import { Children } from 'react';
-import {blog} from './Data/blog';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import Button from 'react-bootstrap/Button';
+// import Card from 'react-bootstrap/Card';
+// import { Children, useState } from 'react';
+// import {blog} from './Data/blog';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 // import { faPhone } from '@fortawesome/free-solid-svg-icons';
+
+
 
 function App() {
 //  const headerInfo = "This is Header Info";
 //   let name="Wscubetech"
+let [status,setStatus]=useState(false);
   return (
-    <div className="main">
-   <button className='bg-[red] p-[10px]'>Save</button>
-   <img width={200} src={weblogo}></img>
-
-   <img src={aboutImg}></img>
-   <Header/>
-   <h1 className='text-[40] text-red-900 font-bold'>welcome to ws</h1>
-
+<div className='App'>
+  <button onClick={()=>setStatus(!status)}>
+  {(status)? 'Hide':'Show'}
+  </button>
+  {
+    (status)
+    ?
+  <p className='para'>welcome to ws</p>
+  :
+  ''
+}
     </div>
   );
 }
