a89d85a1cd25680f82208b602935a667fbced68f
raju15022004 <raju032004@gmail.com>
2025-09-02 09:55:05 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './Header';
- import { useState } from 'react';
+ import Header from './Header';
+//  import { useState } from 'react';
 //  import btnModule from "./Button.module.css"
 // import { ReactNotifications, Store } from 'react-notifications-component'
 // import 'react-notifications-component/dist/theme.css'
@@ -22,61 +22,16 @@ import Header from './Header';
 
 function App() {
 
-let [todolist,setToDolist]=useState([])
-
-  let saveToDoList=(event)=>{
-
-
-    let toname=event.target.toname.value;
-    if(!todolist.includes(toname)){
-    let finalDolist=[...todolist,toname]
-    setToDolist(finalDolist)
-    }
-    else{
-      alert("toDo Name Allready Exists...")
-    }
-    event.preventDefault();
-  }
-  let list=todolist.map((value,index)=>{
-    return(
-      <ToDoListItems  key={index} value={value} indexNumber={index}
-      todolist={todolist}
-      setToDolist={setToDolist}
-      />
-    )
-  })
-
-  return (
-    <div className='App'>
-      <h1>ToDo List</h1>
-      <form onSubmit={saveToDoList}>
-        <input type="text" name='toname'/>
-        <button type='submit'>save</button>
-      </form>
-      <div className='outerDiv'>
-        <ul>
-            {list}
-        </ul>
-
-      </div>
-    </div>
+  return(
+    <>
+    <div className='passwordBox'></div>
+    </>
 
   );
 }
 
 export default App;
 
-function ToDoListItems({value,indexNumber,todolist,setToDolist}){
 
-  let deleteRow=()=>{
-    let finalData=todolist.filter((v,i)=>i!=indexNumber)
-    setToDolist(finalData)
-  }
-  return(
-       <li>
-         {indexNumber+1}{value} <span onClick={deleteRow}>&times;</span>
-       </li>
-  )
-}
 
 
