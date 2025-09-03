87fb5b919abcedcffe8b83af91246f7701da787a
raju15022004 <raju032004@gmail.com>
2025-09-04 00:50:10 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
  import Header from './Header';
-import { useState } from 'react';
+import Category from './Category';
+// import { useState } from 'react';
 
 //  import btnModule from "./Button.module.css"
 // import { ReactNotifications, Store } from 'react-notifications-component'
@@ -19,86 +20,14 @@ import { useState } from 'react';
 // import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 // import { faPhone } from '@fortawesome/free-solid-svg-icons';
 
-const UC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const LC = "abcdefghijklmnopqrstuvwxyz";
-const NC = "0123456789";
-const SC = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
+
 
 
 function App() {
-  let p="Wscubetech";
-
-  let[uppercase,setUppercase]=useState(false)
-  let[lowercase,setLowercase]=useState(false)
-  let[number,setNumber]=useState(false)
-  let[symbols,setSymbols]=useState(false)
-  let[passwordlen,setPasswordLen]=useState(10)
-  let[fPass,setPass]=useState('')
-
-  let createPassword=()=>{
-    let finalPass='';
-    let charSet = '';
-    if (uppercase || lowercase || number || symbols) {
-      if (uppercase) charSet += UC;
-      if (lowercase) charSet += LC;
-      if (number) charSet += NC;
-      if (symbols) charSet += SC;
-      for(let i=0;i=passwordlen;i++){
-        finalPass+=charSet.charAt(Math.floor(Math.random()*charSet.length))
-      }
-      setPass(finalPass);
-
-
-    }
-    else{
-       alert("Plase one CheckBox!.....")
-    }
-  }
-
-  let copyPass=()=>{
-    navigator.clipboard.writeText(fPass)
-  }
 
   return(
     <>
-    <div className='passwordBox'>
-    <h2>Password Generator</h2>
-
-    <div className='passwordBoxin'>
-      <input type='text' value={fPass} readOnly/>
-      <button>Copy</button>
-
-    </div>
-
-    <div className='passlengh'>
-      <lable>Password Length</lable>
-      <input type='number'max={20} min={10} value={passwordlen} onChange={(event)=>setPasswordLen(event.target.value)} />
-    </div>
-
-    <div className='passlengh'>
-      <lable>Include uppercase letters</lable>
-      <input type='checkbox' checked={uppercase} onChange={()=>setUppercase(!uppercase)} />
-    </div>
-
-    <div className='passlengh'>
-      <lable>Include lowercase letters</lable>
-      <input type='checkbox' checked={lowercase} onChange={()=>setLowercase(!lowercase)}/>
-    </div>
-
-    <div className='passlengh'>
-      <lable>Include numbers</lable>
-      <input type='checkbox' checked={number} onChange={()=>setNumber(!number)}/>
-    </div>
-
-    <div className='passlengh'>
-      <lable>Include symbols</lable>
-      <input type='checkbox' checked={symbols} onChange={()=>setSymbols(!symbols)}/>
-    </div>
-
-     <button className='btn' onClick={createPassword}>
-      Generator password
-      </button>
-    </div>
+    <Category/>
     </>
 
   );
